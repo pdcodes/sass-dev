f52989292a430306d676d266fcda675299bd3db9
Peter Dudka <pi.dudka@gmail.com>
2018-12-02 13:21:25 -0500

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 // Packages
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var del = require('del');
 
 // Paths
 
